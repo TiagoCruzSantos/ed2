5f66cbbe1db33c337da06170e5fb48d98cb99945
Tiago da Cruz Santos <tiago.unity.cruz@gmail.com>
2020-09-17 22:17:22 -0300

    automatizated

--- crivo/main.c
+++ crivo/main.c
@@ -4,6 +4,6 @@ int main(int argc, char **argv){
     int k = atoi(argv[1]);
     Celula* lista = init_crivo(k);
     mark_prime_crivo(lista, k);
-    print_prime_crivo(lista, k);
+    //print_prime_crivo(lista, k);
     free_crivo(lista);
 }
