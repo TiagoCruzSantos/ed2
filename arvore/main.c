e8405075d8bc739c06881e2d9557f787ab0db3ae
Tiago da Cruz Santos <tiago.unity.cruz@gmail.com>
2020-09-22 16:19:58 -0300

    vish kkkk

--- arvore/main.c
+++ arvore/main.c
@@ -14,9 +14,9 @@ int main(int argc, char *argv[]){
     gettimeofday(&t, NULL);
     srand(t.tv_usec);
     t_Base* l = init_tree();
-    int v[10] = {6, 2, 7, 1, 4, 3, 5, 9, 8 , 10};
-    for(int i = 0; i < 10; i++){
-        insert_tree(l, v[i]);
+    int n = atoi(argv[1]);
+    for(int i = 0; i < n; i++){
+        insert_tree(l, rand()%1000);
     }
     printf("\n");
     tree_fila_levelOrder(l, printNode);
