b84641f8e79f336ec46eff12604c6f5e83766606
Tiago da Cruz Santos <tiago.unity.cruz@gmail.com>
2020-09-28 04:25:25 -0300

    tem alguma coisa errada em algum lugar eu posso sentir

--- algOrdenação/bubble_sort.c
+++ algOrdenação/bubble_sort.c
@@ -2,7 +2,7 @@
 
 void sort(Item *a, int lo, int hi){
     for(int i = lo + 1; i <= hi; i++){
-        for(int j = i; j >= lo; j--){
+        for(int j = hi; j >= i; j--){
             compexch(a[j - 1], a[j]);
         }
     }
